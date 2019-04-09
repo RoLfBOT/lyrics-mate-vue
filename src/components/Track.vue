db0e7050c0a1a3bb14afdb90639dd428f2dc4327
kokochef <kakoty.kaustabh@gmail.com>
2019-04-10 02:43:51 +0530

    added track component

---
+++ src/components/Track.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="col-6 mb-2 mt-2">
+        <div class="card shadow">
+            <div class="card-body">
+                <h5 class="card-title">{{ track.track.track_name }}</h5>
+                <h6 class="card-subtitle text-muted">{{ track.track.album_name }}</h6>
+                <p class="card-text">{{ track.track.artist_name }}</p>
+                <router-link to="/" class="btn btn-primary btn-block">Get Lyrics</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        track: Object
+    }
+}
+</script>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,25 +1,30 @@
 <template>
     <div class="container h-100">
-        <div class="row h-100 justify-content-center align-items-center">
-          <form class="col-12">
-            <div class="form-group">
-              <label for="formGroupExampleInput">Example label</label>
-              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
-            </div>
-            <div class="form-group">
-              <label for="formGroupExampleInput2">Another label</label>
-              <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
-            </div>
-          </form>   
+        <div class="card mb-2">
+          <div class="card-body">
+              <h3 class="card-title text-center">Search Lyrics</h3>
+              <form class="col-12">
+                <div class="form-group">
+                  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Search by track name">
+                </div>
+              </form>
+              <button class="btn btn-primary btn-block">Search</button>
+          </div>
         </div>
-        <div>
-          {{ track_list }}
+        <div class="row">
+          <Track v-for="track in track_list" :key="track.track_id" :track="track" />
         </div>
     </div>
 </template>
 
 <script>
+import Track from "@/components/Track.vue";
 export default {
+  name: "search",
+  components: {
+    Track
+  },
+
   mounted() {
     this.$store.dispatch("GET_TRACK_LIST");
   },
