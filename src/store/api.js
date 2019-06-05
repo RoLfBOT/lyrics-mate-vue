c23491e288fdf1b66372f6cc01d2c9e94f7904ba
kokochef <kakoty.kaustabh@gmail.com>
2019-06-05 15:58:30 +0530

    api settings changed

--- src/store/api.js
+++ src/store/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1"
+  baseURL: "http://api.musixmatch.com/ws/1.1"
 });
